cfca2850c40b123fc458b8a010af266ea9d7e5aa
marvanessa1 <marvanessa1@live.com>
2021-11-24 02:09:39 -0800

    started on js script

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+var 
+
+var timer;
+var timerCount;
+
+
+
+startButton.addEventListener("click", startQuiz);
