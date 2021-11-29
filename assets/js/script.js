a94edb81ce7e1f04c1d0003bd65c5b650f6f9170
marvanessa1 <marvanessa1@live.com>
2021-11-29 14:42:23 -0800

    added Quiz Start js section and padding to css main sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,63 @@
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
-var 
-
-var timer;
-var timerCount;
-
 var QuizQuestions = [{
     Question: "How many continents are on Earth?",
-    AnswerChoices: [
-        "5",
-        "6",
-        "7",
-        "8"
-    ],
+    AnswerChoices: ["5", "6", "7", "8" ],
     CorrectAnswer: "7"
     },
+
     {
     Question: "The largest island in the world is...",  
-    AnswerChoices: [
-        "New Guinea",
-        "Greenland",
-        "Madagascar",
-        "Victoria Island"
-    ],
+    AnswerChoices: ["New Guinea","Greenland", "Madagascar", "Victoria Island"],
     CorrectAnswer: "Greenland"  
     },
+
     {
     Question: "The largest island in the world is...",  
-    AnswerChoices: [
-        "New Guinea",
-        "Greenland",
-        "Madagascar",
-        "Victoria Island"
-    ],
+    AnswerChoices: ["New Guinea", "Greenland", "Madagascar", "Victoria Island"],
     CorrectAnswer: "Greenland"  
     },
-     {
+
+    {
     Question: "The largest state in the United States is...",  
-    AnswerChoices: [
-        "Texas",
-        "California",
-        "Washington",
-        "Alaska"
-    ],
+    AnswerChoices: ["Texas","California","Washington","Alaska"],
     CorrectAnswer: "Alaska"  
     },
+
     {
     Question: "What is the most common spoken language in the United States after English?",  
-    AnswerChoices: [
-        "Spanish",
-        "Chinese",
-        "Tagalog",
-        "Vietnamese"
-    ],
+    AnswerChoices: ["Spanish","Chinese","Tagalog","Vietnamese"],
     CorrectAnswer: "Spanish"  
     },
+
     {
     Question: "Which ocean is located between North America and Europe",  
-    AnswerChoices: [
-        "Pacific Ocean",
-        "Blue Ocean",
-        "Atlantic Ocean",
-        "Indian Ocean"
-    ],
+    AnswerChoices: ["Pacific Ocean","Blue Ocean","Atlantic Ocean","Indian Ocean"],
     CorrectAnswer: "Atlantic Ocean"  
     },
+
 ]
 
-startButton.addEventListener("click", startQuiz);
+// Start Page
+
+var mainContentEl = document.querySelector('.mainContent');
+
+var QuizInstructions = document.createElement("h3");
+var StartQuiz = document.createElement("button");
+
+QuizInstructions.textContent = "Up for a challenge? You have 75 seconds to finish this quiz regarding word geography.  For razzle dazzle, incorrect answers will dock off 10 seconds from your timer, thus 10 seconds from your score";
+StartQuiz.textContent = "Start";
+
+mainContentEl.appendChild(QuizInstructions);
+mainContentEl.appendChild(StartQuiz);
+
+QuizInstructions.className = "mainContentHeader";
+QuizInstructions.id = "QuizInstructions";
+QuizInstructions.setAttribute("style", "margin:auto; width:75%; padding 15px; line-height: 1.25");
+StartQuiz.id = "StartQuizButton";
+
+var StartQuizButton = document.querySelector('#StartQuizButton');
+var QuizInstructionsEl = document.querySelector('#QuizIntructions');
+
+StartQuizButton.addEventListener("click",timer);
+
+
+
